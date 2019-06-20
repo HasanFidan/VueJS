fe75f2855c1fbc6f0baa2f488a3c6c68afe2ece4
HasanFidan <fidan.hasan@gmail.com>
2019-06-20 18:05:02 +0300

    add new files

---
+++ src/components/History.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="well">
+        <h3>History</h3>
+        <ul class="list-group text-left" >
+            <li class="list-group-item" v-for="type in historyList" v-bind:key="type">>
+                <div>
+                  <strong>{{type}} </strong>
+                </div>
+            </li> 
+
+
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+  props : ["historyList"],
+
+}
+</script>
